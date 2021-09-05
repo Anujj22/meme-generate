c2e7ef44a50eed76697c0220e7bd2f6190190725
Anujj22 <90130651+Anujj22@users.noreply.github.com>
2021-09-05 14:12:09 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import SearchBox from "./SearchBox"
+import Memes from "./Memes"
+
+function App() {
+  return (
+    <div>
+      <SearchBox />
+
+
+
+      <Memes />
+    </div>
+  )
+}
+
+export default App;
